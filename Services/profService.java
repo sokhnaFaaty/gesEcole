7d190cbc7fb080f6852a75141e3be9dfd91c05f9
nexuusdev <mamefat2004@gmail.com>
2026-06-17 01:16:28 +0200

    commit

--- Services/profService.java
+++ Services/profService.java
@@ -1,8 +1,8 @@
 package GesEcole.Services;
-import Entity.Prof;
+import GesEcole.Entity.Prof;
 public class profService {
     Prof[] profs=new Prof[100];
-    int nbProfs=0;
+    public int nbProfs=0;
     
     public void ajouter(Prof p){
         profs[nbProfs]=p;
