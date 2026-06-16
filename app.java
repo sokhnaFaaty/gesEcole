b10c8e4f7779771fe9a9a9cd3fb8bfb1e8479427
nexuusdev <mamefat2004@gmail.com>
2026-06-17 00:16:47 +0200

    lister Profs

--- app.java
+++ app.java
@@ -1,30 +1,32 @@
 package GesEcole;
 
-
-    import Views.profView;
+import Views.profView;
 import java.util.Scanner;
- 
+
 public class app {
     public static void main(String[] args) {
- 
+
         profView pView = new profView();
         Scanner sc = new Scanner(System.in);
         int choix;
- 
+
         do {
             System.out.println("\n=== MENU GES ECOLE ===");
             System.out.println("1. Ajouter un Prof");
             System.out.println("6. Quitter");
             System.out.print("Votre choix : ");
             choix = sc.nextInt();
- 
+
             if (choix == 1) {
                 pView.ajouterProf();
             }
- 
+
+            if (choix == 3) {
+                pView.afficherProfs();
+            }
+
         } while (choix != 6);
- 
+
         System.out.println("Au revoir !");
     }
 }
- 
