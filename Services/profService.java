8f7ec98078bf56d000e0c4ab91d94f8097152fcc
nexuusdev <mamefat2004@gmail.com>
2026-06-17 00:00:49 +0200

    ajouter un prof

--- Services/profService.java
+++ Services/profService.java
@@ -1,5 +1,10 @@
 package GesEcole.Services;
-
+import Entity.Prof;
 public class profService {
-    
+    Prof[] profs=new Prof[100];
+    int nbProfs=0;
+    public void ajouter(Prof p){
+        profs[nbProfs]=p;
+        nbProfs++;
+    }
 }
--- Views/profView.java
+++ Views/profView.java
@@ -1,5 +1,38 @@
 package GesEcole.Views;
-
+import Entity.Prof;
+import Services.profService;
+import java.util.Scanner;
+ 
 public class profView {
-    
+profService service =new profService();
+Scanner sc=new Scanner(system.in);
+
+//methode pour a=saisir et ajouter un prof 
+public void ajouterProf(){
+    System.out.println("--- Ajouter un Professeur ---");
+ 
+        System.out.print("Nom        : ");
+        String nom = sc.nextLine();
+ 
+        System.out.print("Prénom     : ");
+        String prenom = sc.nextLine();
+ 
+        System.out.print("Email      : ");
+        String email = sc.nextLine();
+ 
+        System.out.print("Grade      : ");
+        String grade = sc.nextLine();
+ 
+        System.out.print("Spécialité : ");
+        String specialite = sc.nextLine();
+
+        // On crée l'objet Prof
+        Prof p = new Prof(nom, prenom, email, grade, specialite);
+ 
+        // On l'envoie au service pour le stocker
+        service.ajouter(p);
+ 
+        System.out.println("Prof ajouté avec succès !");
+
+}
 }
